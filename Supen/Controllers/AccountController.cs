91aa2f1d9afdf914328c09e28337b0f7902db0d8
marcusbatan <markus_batan@hotmail.com>
2020-10-15 10:05:01 +0200

    jobbar inlogg

---
+++ Supen/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Supen.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AppUser appuser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SupenEntities())
+                {
+                    var obj = db.AppUser.Where(a => a.Email.Equals(appuser.Email) && a.Password.Equals(appuser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.AppUserId.ToString();
+                        Session["Email"] = obj.Email.ToString();
+                        return RedirectToAction("Home");
+                    }
+                }
+            }
+            return View(appuser);
+        }
+
+    }
+}
