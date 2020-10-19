13ad004dc1b4fb151909e1e13b57d286f91f4174
marcusbatan <markus_batan@hotmail.com>
2020-10-19 11:30:51 +0200

    update

---
+++ Database/GameRepos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class GameRepos
+    {
+        public Games addGames(int id, string hometeam, string awayteam, int homescore, int awayscore, int teamId)
+        {
+            using (var db = new SupenEntities())
+            {
+                var addGame = new Games
+                {
+                    Id = id,
+                    HomeTeam = hometeam,
+                    AwayTeam = awayteam,
+                    HomeScore = homescore,
+                    AwayScore = awayscore,
+                    TeamId = teamId
+                };
+                db.Games.Add(addGame);
+                db.SaveChanges();
+                return addGame;
+            }
+        }
+    }
+}
---
+++ Supen/Controllers/GameController.cs
@@ -0,0 +1,41 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Supen.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int id, string hometeam, string awayteam, int homescore, int awayscore, int teamId)
+        {
+            using (var db = new SupenEntities())
+            {
+                var repos = new GameRepos();
+                repos.addGames(id, hometeam, awayteam, homescore, awayscore, teamId);
+            }
+            return View();
+        }
+        public ActionResult fillCmb()
+        {
+            var db = new SupenEntities();
+            var teamList = db.TeamSet.ToList();
+            SelectList list = new SelectList(teamList, "Id", "TeamName");
+            ViewBag.listofteams = list;
+            return View();
+
+        }
+    }
+}
