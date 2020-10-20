ba035fe060ac92637e8d43e9bfd2884c60219e68
marcusbatan <markus_batan@hotmail.com>
2020-10-20 08:57:31 +0200

    games

--- Supen/Controllers/GameController.cs
+++ Supen/Controllers/GameController.cs
@@ -1,10 +1,12 @@
 ﻿using Database;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
@@ -13,29 +15,20 @@ namespace Supen.Controllers
 {
     public class GameController : Controller
     {
-        // GET: Game
         public ActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Index(int id, string hometeam, string awayteam, int homescore, int awayscore, int teamId)
+        public ActionResult Index(Games game)
         {
             using (var db = new SupenEntities())
             {
-                var repos = new GameRepos();
-                repos.addGames(id, hometeam, awayteam, homescore, awayscore, teamId);
+                var teams = new Team();
+                ViewBag.Team = db.TeamSet.ToList();
+                ViewBag.Team2 = db.TeamSet.ToList();
+                return View();
             }
-            return View();
-        }
-        public ActionResult fillCmb()
-        {
-            var db = new SupenEntities();
-            var teamList = db.TeamSet.ToList();
-            SelectList list = new SelectList(teamList, "Id", "TeamName");
-            ViewBag.listofteams = list;
-            return View();
-
         }
     }
 }
